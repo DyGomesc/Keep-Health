9b62930177599882ca3167b16abfc2d0f6b7b2a1
Randy Gomes <raandy.gc@gmail.com>
2024-03-25 21:24:29 -0300

    first commit

---
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'app-default-login-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './default-login-layout.component.html',
+  styleUrl: './default-login-layout.component.css'
+})
+export class DefaultLoginLayoutComponent {
+  @Input() tittle: string = "";
+  @Input() primaryBtnText: string = "";
+  @Input() secondaryBtnText: string = "";
+  @Output("submit") onSubmit = new EventEmitter();
+
+  submit(){
+    this.onSubmit.emit();
+  }
+
+}
