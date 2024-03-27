03c1cccdef1d0eb3e752343db5f5da52baa38d7f
Randy Gomes <raandy.gc@gmail.com>
2024-03-26 22:27:10 -0300

    feat: atualização pasta Shared

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -14,8 +14,14 @@ export class DefaultLoginLayoutComponent {
   @Input() secondaryBtnText: string = "";
   @Output("submit") onSubmit = new EventEmitter();
 
+  @Output("navigate") onNavigate = new EventEmitter();
+
   submit(){
     this.onSubmit.emit();
+  }
+
+  navigate(){
+    this.onNavigate.emit();
   }
 
 }
